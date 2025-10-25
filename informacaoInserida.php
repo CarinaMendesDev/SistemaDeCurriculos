cb06e92b1444af27406e5965151dcfa4a2e053e6
CarinaMendes <190530193+CarinaMendesDev@users.noreply.github.com>
2025-10-25 05:59:58 -0300

    Sistema de Currículos — Fichário Digital (PHP + MVC)
    Inclusão dos arquivos

---
+++ informacaoInserida.php
@@ -0,0 +1,7 @@
+<form action="" method="post" class="w3-container w3-card-4 w3-light-grey w3-text-blue w3-margin w3-display-middle" style="width:30%;">
+  <div class="w3-row w3-section">
+    <button name="btnInfInserir" class="w3-button w3-block w3-margin w3-blue w3-round-large" style="width:90%;">
+      Inclusão Realizada com Sucesso!
+    </button>
+  </div>
+</form>
